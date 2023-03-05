fb77003444f1ffea5e69472092741484907bfcee
Tung Dinh <dinhviettung1011@gmail.com>
2023-03-05 16:39:01 -0500

    Initial Commit

---
+++ Flixter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixter
+//
+//  Created by Dinh Viet Kien on 05/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
